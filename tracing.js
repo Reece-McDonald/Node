5a68e6e5cedf0aa8eb57530e464825f7aa526627
Reece McDonald <mcdonaldreece@ufl.edu>
2023-11-26 23:37:19 -0500

    final push

--- tracing.js
+++ tracing.js
@@ -1,7 +1,8 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+//const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-node");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
 //Instrumentations
@@ -9,23 +10,24 @@ const { ExpressInstrumentation } = require("opentelemetry-instrumentation-expres
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const {JaegerExporter} = require("@opentelemetry/exporter-jaeger");
 //Exporter
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
-   const provider = new NodeTracerProvider({
-       resource: new Resource({
-           [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-       }),
-   });
-   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-   provider.register();
-   registerInstrumentations({
-       instrumentations: [
-           new HttpInstrumentation(),
-           new ExpressInstrumentation(),
-           new MongoDBInstrumentation(),
-       ],
-       tracerProvider: provider,
-   });
-   return trace.getTracer(serviceName);
-};
+    const exporter = new JaegerExporter();
+    const provider = new NodeTracerProvider({
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+        }),
+    });
+    provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+    provider.register();
+    registerInstrumentations({
+        instrumentations: [
+            new HttpInstrumentation(),
+            new ExpressInstrumentation(),
+            new MongoDBInstrumentation(),
+        ],
+        tracerProvider: provider,
+    });
+    return trace.getTracer(serviceName);
+ };
